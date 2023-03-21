1637a3092a356e041a2b8b1c3d58ea7f6aac1ca2
3yude <lowfhon@outlook.com>
2023-03-21 22:40:31 +0800

    build: add makefile

--- scull/main.c
+++ scull/main.c
@@ -5,12 +5,19 @@
 /* for dev_t class */
 #include <linux/types.h>
 
+/* register and unregister device */
+#include <linux/fs.h>
+
 /* for operating dev_t */
 #include <linux/kdev_t.h>
 
 /* for error code */
 #include <linux/errno.h>
 
+/* for cdev */
+#include <linux/cdev.h>
+
+#include "scull.h"		/* local definitions */
 
 int scull_minor =   0;
 int scull_nr_devs = SCULL_NR_DEVS;	/* number of bare scull devices */
@@ -31,11 +38,12 @@ int __init scull_init_module(void) {
 }
 
 
-int __exit scull_cleanup_module(void) {
+void __exit scull_cleanup_module(void) {
 
 	dev_t devno = MKDEV(scull_major, scull_minor);
 
 	unregister_chrdev_region(devno, scull_nr_devs);
+
 }
 
 module_init(scull_init_module);
